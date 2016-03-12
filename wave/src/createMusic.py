8875a714166b86719c6599c81ae7342a8c2d375c
J-Holliday <14ec004@ms.dendai.ac.jp>
2016-03-12 15:28:39 +0900

    sop + bas
    add bas part

--- wave/src/createMusic.py
+++ wave/src/createMusic.py
@@ -3,7 +3,7 @@ import struct
 
 # define header
 RIFF_HEADER = b"\x52\x49\x46\x46"   # "RIFF"
-FILE_SIZE = b"\x9C\xD7\12\00"       # tekitou
+FILE_SIZE = b"\xAC\x88\15\00"       # tekitou
 WAVE_HEADER = b"\x57\x41\x56\x45"   # "WAVE"
 WAVE_FORMAT = b"\x66\x6D\x74\x20"   # "fmt "
 FORMAT_SIZE = b"\x10\x00\x00\x00"   # 16
@@ -14,21 +14,25 @@ BYTES_PER_SEC = b"\x88\x58\x01\x00" # 44100 * 2(16bit) * 1(mono)
 BLOCK_SIZE = b"\x02\x00"            # 2(16bit) * 1(mono)
 SIGNIFICANT = b"\x10\x00"           # 16bit
 DATA_HEADER = b"\x64\x61\x74\x61"   # "data"
-DATA_SIZE = b"\x9C\xD7\x12\x00"     # tekitou
+DATA_SIZE = b"\xAC\x88\x15\x00"     # tekitou
 
 HEADER = RIFF_HEADER + FILE_SIZE + WAVE_HEADER + WAVE_FORMAT + FORMAT_SIZE + FORMAT_ID + CHUNNELS + SAMPLING_RATE + BYTES_PER_SEC + BLOCK_SIZE + SIGNIFICANT + DATA_HEADER + DATA_SIZE
 
 # make wave
 A = 32767.0
 fs = 44100
-f0 = [262, 294, 330, 349, 392, 440, 494, 523]
+f0 = np.array([262, 294, 330, 349, 392, 440, 494, 523])
+f1 = f0/2
+f2 = f0/4
 wave = []
-twincleStar = [0, 0, 4, 4, 5, 5, 4, 3, 3, 2, 2, 1, 1, 0]
+sop = [0, 0, 4, 4, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0]
+bas = [0, 0, 2, 0, 3, 0, 2, 0, 1, 1, 0, 0, 4, 4, 0, 0]
 
-for i in twincleStar:
+for i in range(len(sop)):
   for n in range(fs):
-    f = A*np.sin(2*np.pi*f0[i]*n/fs)
-    wave.append(int(f))
+    fa = A/2*np.sin(2*np.pi*2*f0[sop[i]]*n/fs)
+    fb = A/2*np.sin(2*np.pi*2*f1[bas[i]]*n/fs)
+    wave.append(int(fa+fb))
 
 # convert wave to binary data
 binary =  struct.pack("h" * len(wave), *wave)
